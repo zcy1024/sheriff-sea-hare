00a60b87595173e8f3700163ea6288ab8864fd7a
Zcy <zcy_chao1024@163.com>
2025-05-13 21:38:09 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'mainnet-aggregator.hoh.zone',
+                port: '',
+                pathname: '/v1/blobs/*',
+                search: '',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
